16908642b07c0afd98079ac55a91813ab83edc3d
mattyvinx <matthewvinca1227@gmail.com>
2022-09-28 19:46:24 -0400

    Added welcome line, reads in coordinate pair

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,12 +4,15 @@ import java.util.*;
 public class HelloWorld {
     public static void main(String[] args) {
        // Prints "Hello, World" in the terminal window.
+       Scanner scanny = new Scanner(System.in);
+       int x, y;
        String[][] row1 = new String[5][5];
        initBoard(row1);
+       System.out.println("\nWelcome to Conenct 4! First to get 4 in a row wins :D\n\n");
        displayGameBoard(row1);
-       row1[4][4] = "R";
-       row1[2][3] = "R"; 
-       displayGameBoard(row1);
+       System.out.println("Please give a coordinate pair for row\n0-4 and column 0-4: ");
+       x = scanny.nextInt();
+       y = scanny.nextInt();
        //System.out.printf("%-5s %-5s %-5s %-5s %-5s\n", row1[0], row1[1], row1[2], row1[3], row1[4]);
          
       //System.out.printf("%-5s %-5s %-5s %-5s %-5s\n", row1[0], row1[1], row1[2], row1[3], row1[4]);
@@ -17,20 +20,16 @@ public class HelloWorld {
     }
     public static void initBoard(String[][] gameBoard){
       for (int i =0; i<gameBoard.length; i++){
-         gameBoard[i][0] = "X";
-         gameBoard[i][1] = "X";
-         gameBoard[i][2] = "X";
-         gameBoard[i][3] = "X";
-         gameBoard[i][4] = "X";
+         gameBoard[i][0] = "O";
+         gameBoard[i][1] = "O";
+         gameBoard[i][2] = "O";
+         gameBoard[i][3] = "O";
+         gameBoard[i][4] = "O";
       }
     }
     public static void displayGameBoard(String[][] gameBoard){
       for (int i =0; i<gameBoard.length; i++){
          System.out.printf("%-5s %-5s %-5s %-5s %-5s\n", gameBoard[i][0], gameBoard[i][1], gameBoard[i][2],gameBoard[i][3],gameBoard[i][4]);
-         /*gameBoard[i][1] = "X";
-         gameBoard[i][2] = "X";
-         gameBoard[i][3] = "X";
-         gameBoard[i][4] = "X";*/
       }
       System.out.println("");
 
