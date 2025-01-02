bfbf099b1f4d06f288ff930f5fe3608d5ae77122
Matthew Vinca <matthewvinca1227@gmail.com>
2025-01-01 20:23:54 -0500

    add hellword.py to repo

---
+++ helloword.py
@@ -0,0 +1,9 @@
+# Write 'Hello, World' to standard output.
+print('Please enter a number to increase by 5')
+x = float(input())
+x += 5
+print(int(x))
+print('Great work! Now, how about one to divide this number by')
+y = float(input())
+print(x/y)
+
