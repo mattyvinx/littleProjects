4d85293515847edd3c70aeccd335b954b79c4aeb
Matthew Vinca <matthewvinca1227@gmail.com>
2025-01-01 20:44:19 -0500

    This is a new program.

---
+++ ChatGPT Projects/simpleHello.py
@@ -0,0 +1,10 @@
+'''
+Objective:
+Write a Python script that:
+
+Asks the user for their name.
+Greets the user with their name.
+'''
+print('Hi, what\'s your name?')
+name = str(input())
+print('Hi,'+name+' nice to meet you :D')
