2dbb398312df4dc36fd8c3077771923c16489bdc
Matthew Vinca <matthewvinca1227@gmail.com>
2025-01-01 20:26:37 -0500

    updated to increase by 10

--- helloword.py
+++ helloword.py
@@ -1,7 +1,7 @@
 # Write 'Hello, World' to standard output.
-print('Please enter a number to increase by 5')
+print('Please enter a number to increase by 10')
 x = float(input())
-x += 5
+x += 10
 print(int(x))
 print('Great work! Now, how about one to divide this number by')
 y = float(input())
