cee66f0ed4b65dcda44cbd00650c69e4d68ad2a9
Matthew Vinca <matthewvinca1227@gmail.com>
2025-02-11 12:10:35 -0500

    midday update

--- Vangaurd/webRead.py
+++ Vangaurd/webRead.py
@@ -4,11 +4,16 @@ import plotly.graph_objects as mapper
 from dotenv import load_dotenv
 import os
 from datetime import datetime
+import time
 
 load_dotenv()
 
 apiKey = os.getenv("apiKey")
 
+Northeast = ["ME", "NH", "VT", "MA", "CT", "RI", "NJ", "NY", "PA"]
+South = ["WV", "MD", "DE", "DC", "VA", "WV", "KY", "TN", "NC", "SC", "GA", "FL", "MS", "AL", "AR", "OK", "LA", "TX"]
+Midwest = ["ND", "MN", "WI", "MI", "SD", "IA", "IL", "IN", "OH", "NE", "MO", "KS"]
+West = ["WA", "ID", "MT", "OR", "WY", "CA", "NV", "UT", "CO", "AZ", "NM"]
 
 tablePop = pd.read_html('https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population')
 populationTable = tablePop[2]
@@ -24,49 +29,64 @@ for i in range(50): ##for the top 50 rows, pulls coordinates. Coordinates then u
     populationTable.loc[i, 'Longitude Decimal'] = Lon
     cityString = populationTable.iloc[i]["City"].item() #clears special marks from city names.
     cityStringCleaned = cityString.split("[")[0] 
-    populationTable.loc[i, "City Plain Text"] = cityStringCleaned #stores cleaned names 
+    populationTable.loc[i, "City Plain Text"] = cityStringCleaned #stores cleaned names
+    if populationTable.iloc[i]["ST"].item() in Northeast:
+        populationTable.loc[i, "Region"] = "Northeast"
+    elif populationTable.iloc[i]["ST"].item() in South:
+        populationTable.loc[i, "Region"] = "South"
+    elif populationTable.iloc[i]["ST"].item() in Midwest:
+        populationTable.loc[i, "Region"] = "Midwest"
+    elif populationTable.iloc[i]["ST"].item() in West:
+        populationTable.loc[i, "Region"] = "West"
+    
 
     response = rq.get(f"http://api.openweathermap.org/data/2.5/weather?lat={Lat}&lon={Lon}&appid={apiKey}&units=imperial") ##call openweather API, get current weather.
     data = response.json()
 
     if response.status_code == 200: ##store current weather attributes
         temperature = data["main"]["temp"] 
+        feelsLike = data["main"]["feels_like"]
         description = data["weather"][0]["description"]
         humidity = data["main"]["humidity"]
         wind_speed = data["wind"]["speed"]
         ## Append weather info to dataframe. To be used by dashboard.
         populationTable.loc[i, 'Temperature'] = temperature
+        populationTable.loc[i, 'Feels Like Temp'] = feelsLike
         populationTable.loc[i, 'Description'] = description
         populationTable.loc[i, 'Humidity'] = humidity
         populationTable.loc[i, 'Wind Speed'] = wind_speed
         populationTable.loc[i, 'Refresh Time'] = datetime.now()
     else: ##errors added if issue calling API.
-        print("Error:", data.get("message", "Failed to retrieve data"))
         populationTable.loc[i, 'Temperature'] = 'error'
-        populationTable.loc[i, 'Demperature'] = 'error'
-        populationTable.loc[i, 'Humidity'] = 'error'
-        populationTable.loc[i, 'Wind Speed'] = 'error'
-    
-    ##Daily historical average, used for dashboard.
-    presentDateO = datetime.now()
-    presentDayU = presentDateO.strftime("%Y-%m-%d")
-    response = rq.get(f"https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={Lat}&lon={Lon}&dt={presentDayU}&appid={apiKey}")
-    data = response.json()
+''' ##pulling historical averages
+    presentTime = datetime.now()
+    presentMonth, presentDay, presentHour = presentTime.month, presentTime.day, presentTime.hour
 
+    years = [presentTime.year - i for i in range(1, 6)]
+    timeStampsUNIX = [int(time.mktime(datetime(year, presentMonth, presentDay, presentHour, 0).timetuple())) for year in years]
 
-    if response.status_code == 200:
-        avgLowK = data["result"]["temp"]["average_min"] ##currently in K
-        avgHighK = data["result"]["temp"]["average_max"]  ##currently in K
-        avgTempK = ((avgLowK+avgHighK) / 2) ##calculate current daily average
-        avgTempF = ((avgTempK - 273.15)*9/5+32) ##convert to F
-        print(avgTempF)
-        populationTable.loc[i, 'Daily Avereage Temp'] = avgTempF
-    else:
-        print(response)
-
-        ##Append values to dataframe. To be used by dashboard as well.
-
-
+    for tsu in timeStampsUNIX:
+        response = rq.get(f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={Lat}&lon={Lon}&dt={tsu}&appid={apiKey}")
+        data = response.json()
+        avgTempL = []
+        if response.status_code == 200:
+            if "data" in data and len(data["data"]) > 0:
+                tempK = data['data'][0]['temp']
+                tempF = (tempK - 273.15) * 9/5 + 32
+                avgTempL.append(tempF)
+                avgTemp = 0
+                tempSum = 0
+                for temp in avgTempL:
+                    tempSum+=temp
+                avgTemp = tempSum/5
+                populationTable.loc[i, 'Average Temp Last 5'] = avgTemp
+            else:
+                print("Data not available for given time")
+                populationTable.loc[i, 'Average Temp Last 5'] = 'N/A'
+        else:
+            print("Response Error")
+            populationTable.loc[i, 'Average Temp Last 5'] = 'N/A'
+'''
 
 
 errorPresent = False #validate pull successful. If not, error will be displayed. 
